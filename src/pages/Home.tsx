7fbd3a5f30d0f49af2d453faace499d1fa1a4f0d
gureumwoon <vfrvfr3221@gmail.com>
2024-03-02 16:27:52 +0900

    Home 페이지에 Header컴포넌트 추가 & Outlet추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+
 export default function Home() {
   return (
-    <div>HomePage</div>
+    <>
+      <Header />
+      <Outlet />
+    </>
   )
 }
