df94663db92a356654c2443b898ab1c64c62c177
gureumwoon <vfrvfr3221@gmail.com>
2024-03-04 19:47:27 +0900

    queryClient를 다른 컴포넌트에서 사용할 수 있도록 설정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+
+const queryClient = new QueryClient();
 export default function Home() {
   return (
     <>
       <Header />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
     </>
   )
 }
