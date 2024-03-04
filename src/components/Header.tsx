605a8093f4715b34dd369deed71aa79c1dc860dc
gureumwoon <vfrvfr3221@gmail.com>
2024-03-04 19:46:43 +0900

    키워드 검색 시 keyword쿼리를 가진 주소로 이동하도록 설정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,17 @@
+import { FormEvent, useState } from 'react';
 import { FaYoutube } from 'react-icons/fa';
 import { GoSearch } from 'react-icons/go'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Header() {
+    const navigate = useNavigate();
+    const [keyword, setKeyword] = useState('');
+
+    const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        navigate(`/videos/${keyword}`);
+    }
+
     return (
         <header className='w-full flex justify-between items-center px-4 py-3'>
             <Link to='/'>
@@ -11,8 +20,8 @@ export default function Header() {
                     <h1 className='text-white text-2xl font-semibold'>YouTube</h1>
                 </div>
             </Link>
-            <form className='w-full flex justify-center flex-grow'>
-                <input type='text' placeholder='검색' className='w-3/12 min-w-96 py-2 px-3 rounded-tl-full rounded-bl-full border border-borderCorlor bg-mainColor z-10' />
+            <form onSubmit={handleSearch} className='w-full flex justify-center flex-grow'>
+                <input type='text' placeholder='검색' onChange={(e) => setKeyword(e.target.value)} value={keyword} className='w-3/12 min-w-96 py-2 px-3 rounded-tl-full rounded-bl-full border border-borderCorlor bg-mainColor z-10' />
                 <button className='bg-searchBtnColor w-16 h-10 rounded-tr-full rounded-br-full'>
                     <GoSearch className='text-subColor text-xl m-auto' />
                 </button>
