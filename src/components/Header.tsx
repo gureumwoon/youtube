25b38946f0802fa4c8006aa6d37dc4bd226cbab9
gureumwoon <vfrvfr3221@gmail.com>
2024-03-05 20:18:27 +0900

    useParams로 query가져오고 query파라미터에 따라 url주소를 변경시켜줌

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,21 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import { FaYoutube } from 'react-icons/fa';
 import { GoSearch } from 'react-icons/go'
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 export default function Header() {
+    const { keyword } = useParams();
     const navigate = useNavigate();
-    const [keyword, setKeyword] = useState('');
+    const [searchKeyword, setSearchKeyword] = useState('');
 
     const handleSearch = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        navigate(`/videos/${keyword}`);
+        navigate(`/videos/${searchKeyword}`);
     }
+
+    useEffect(() => {
+        setSearchKeyword(keyword || '');
+    }, [keyword])
 
     return (
         <header className='w-full flex justify-between items-center px-4 py-3'>
@@ -21,7 +26,7 @@ export default function Header() {
                 </div>
             </Link>
             <form onSubmit={handleSearch} className='w-full flex justify-center flex-grow'>
-                <input type='text' placeholder='검색' onChange={(e) => setKeyword(e.target.value)} value={keyword} className='w-3/12 min-w-96 py-2 px-3 rounded-tl-full rounded-bl-full border border-borderCorlor bg-mainColor z-10' />
+                <input type='text' placeholder='검색' onChange={(e) => setSearchKeyword(e.target.value)} value={searchKeyword} className='w-3/12 min-w-96 py-2 px-3 rounded-tl-full rounded-bl-full border border-borderCorlor bg-mainColor z-10' />
                 <button className='bg-searchBtnColor w-16 h-10 rounded-tr-full rounded-br-full'>
                     <GoSearch className='text-subColor text-xl m-auto' />
                 </button>
