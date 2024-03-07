2b123215b1e63f42432cc550e0eaabe64a94b4f5
gureumwoon <vfrvfr3221@gmail.com>
2024-03-07 19:15:29 +0900

    메인 Home페이지의 video카드에 title,thumbnails,channelTitle,publishedAt시각화 하기

--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -1,4 +1,5 @@
-import { Video } from "../pages/Videos";
+import { Video } from "../../api/YoutubeApi";
+
 
 interface VideoListProps {
     vid: Video; // Video 인터페이스를 타입으로 지정
@@ -6,9 +7,16 @@ interface VideoListProps {
 
 export default function VideoList({ vid }: VideoListProps) {
 
+    const { title, thumbnails, channelTitle, publishedAt } = vid.snippet;
+
     return (
-        <div>
-            {vid.snippet.title}
-        </div>
+        <li>
+            <img src={thumbnails.medium.url} alt={title} />
+            <div>
+                <p>{title}</p>
+                <p>{channelTitle}</p>
+                <p>{publishedAt}</p>
+            </div>
+        </li>
     )
 }
