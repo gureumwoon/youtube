1ef896d041d0085efa3c03e4fd4fd2669b64a702
gureumwoon <vfrvfr3221@gmail.com>
2024-03-07 19:16:47 +0900

    context사용을 위해 하위 컴포넌트들을 YoutuveApiProvider로 감싸기

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { YoutubeApiProvider } from "../context/YoutubeApiContext";
 
 const queryClient = new QueryClient();
 export default function Home() {
   return (
     <>
       <Header />
-      <QueryClientProvider client={queryClient}>
-        <Outlet />
-      </QueryClientProvider>
+      <YoutubeApiProvider>
+        <QueryClientProvider client={queryClient}>
+          <Outlet />
+        </QueryClientProvider>
+      </YoutubeApiProvider>
     </>
   )
 }
