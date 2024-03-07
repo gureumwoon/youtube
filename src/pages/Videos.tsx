8e2f57bfcb02ae09f5ba016d7a4c1bee5000c4c4
gureumwoon <vfrvfr3221@gmail.com>
2024-03-07 21:41:00 +0900

    전체 videos카드들을 grid형식으로 정렬 & 화면의 넓이에 따라 col적용

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -18,7 +18,7 @@ export default function Videos() {
         <>
             {isLoading && <p>Loading...</p>}
             {error && <p>Something wrong...</p>}
-            {videos && <ul>
+            {videos && <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4">
                 {
                     videos.map((vid: Video) => {
                         return <VideoList key={vid.id} vid={vid} />
