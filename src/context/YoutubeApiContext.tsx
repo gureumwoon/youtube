64f49ca77e99eadba9711ec0acf5717a8522c05d
gureumwoon <vfrvfr3221@gmail.com>
2024-03-08 22:56:20 +0900

    value데이터 mock data에서 실제 data로 변경

--- src/context/YoutubeApiContext.tsx
+++ src/context/YoutubeApiContext.tsx
@@ -1,16 +1,16 @@
 import { ReactNode, createContext } from "react";
-// import YoutubeApi from "../../api/YoutubeApi";
-// import YoutubeClient from "../../api/YoutubeClient";
-import FakeClient from "../../api/FakeClient";
+import YoutubeApi from "../../api/YoutubeApi";
+import YoutubeClient from "../../api/YoutubeClient";
+// import FakeClient from "../../api/FakeClient";
 
-export const YoutubeApiContext = createContext<FakeClient | null>(null);
-const client = new FakeClient();
-// const client = new YoutubeClient();
-// const youtube = new YoutubeApi(client);
+export const YoutubeApiContext = createContext<YoutubeApi | null>(null);
+// const client = new FakeClient();
+const client = new YoutubeClient();
+const youtube = new YoutubeApi(client);
 
 export function YoutubeApiProvider({ children }: { children: ReactNode }) {
     return (
-        <YoutubeApiContext.Provider value={client}>
+        <YoutubeApiContext.Provider value={youtube}>
             {children}
         </YoutubeApiContext.Provider>
     )
