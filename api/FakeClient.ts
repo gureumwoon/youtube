5bdcccd80a0d8f3eaecb1085ecf7bc3309269513
gureumwoon <vfrvfr3221@gmail.com>
2024-03-08 21:59:27 +0900

    FakeClient에 channel이미지 url가지고 오는 함수 생성

--- api/FakeClient.ts
+++ api/FakeClient.ts
@@ -1,13 +1,22 @@
 import axios, { AxiosResponse } from "axios";
+import { YoutubeSearchApiResponseData } from "./YoutubeApi";
 
 export default class FakeClient {
 
     async search() {
-        return axios.get('http://localhost:5173/keyWord.json')
+        return axios.get('http://localhost:5173/KeyWord.json')
+            .then((res: AxiosResponse<YoutubeSearchApiResponseData>) => res.data.items
+                .map((item) => ({ ...item, id: item.id.videoId })))
     }
 
     async videos() {
         return axios.get('http://localhost:5173/HotTrendVideo.json')
             .then((res: AxiosResponse) => res.data.items);
     }
+
+    async channels() {
+        return axios.get('http://localhost:5173/ChannelInfo.json')
+            .then((res: AxiosResponse) => res.data.items[0].snippet.thumbnails.default.url);
+    }
+
 }
