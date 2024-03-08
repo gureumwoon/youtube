9e9cd406292d5173c032b4890361f9afb2d9be06
gureumwoon <vfrvfr3221@gmail.com>
2024-03-08 22:03:59 +0900

    channel이미지와 이름 나타내는 ChannelInfo컴포넌트 생성

---
+++ src/components/ChannelInfo.tsx
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { useYoutubeApi } from "../context/useYoutubeApi"
+
+export interface ChannelInfoProps {
+    id: string;
+    name?: string;
+}
+
+export default function ChannelInfo({ id, name }: ChannelInfoProps) {
+    const youtube = useYoutubeApi();
+    const { data: url } = useQuery({
+        queryKey: ['channel', id],
+        queryFn: () => youtube?.channels()
+    })
+
+    return (
+        <div className="flex my-4 mb-8 items-center">
+            {url && <img className='w-10 h-10 rounded-full' src={url} alt={name} />}
+            <p className="text-lg font-medium ml-2">{name}</p>
+        </div>
+    )
+}
