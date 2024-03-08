f3b73ed819295aedbbafd943e5a55b41378ddf77
gureumwoon <vfrvfr3221@gmail.com>
2024-03-08 22:06:31 +0900

    list type에 따라 스타일링 변화

--- src/components/VideoList.tsx
+++ src/components/VideoList.tsx
@@ -1,21 +1,27 @@
+import { useNavigate } from "react-router-dom";
 import { Video } from "../../api/YoutubeApi";
 import { convertDate } from "../util/date";
 
 interface VideoListProps {
     vid: Video; // Video 인터페이스를 타입으로 지정
+    type?: string;
 }
 
-export default function VideoList({ vid }: VideoListProps) {
-
+export default function VideoList({ vid, type }: VideoListProps) {
+    const navigate = useNavigate();
+    const isList = type === 'list';
     const { title, thumbnails, channelTitle, publishedAt } = vid.snippet;
 
     return (
-        <li>
-            <img className='w-full' src={thumbnails.medium.url} alt={title} />
+        <li
+            className={isList ? 'flex gap-1 m-2' : ''}
+            onClick={() => navigate(`/videos/watch/${vid.id}`, { state: { vid } })}
+        >
+            <img className={isList ? 'w-60 mr-2' : 'w-full'} src={thumbnails?.medium.url} alt={title} />
             <div>
                 <p className="font-semibold my-2 line-clamp-2">{title}</p>
                 <p className="text-sm opacity-80">{channelTitle}</p>
-                <p className="text-sm opacity-80">{convertDate(publishedAt)}</p>
+                <p className="text-sm opacity-80">{convertDate(publishedAt || '')}</p>
             </div>
         </li>
     )
