9725d3fc78af289f3325c2761df1604eca109640
gureumwoon <vfrvfr3221@gmail.com>
2024-03-08 22:08:30 +0900

    video상세페이지 오른쪽 channel연관 비디오 보여주는 RelatedVideo컴포넌트 생성

---
+++ src/components/RelatedVideos.tsx
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+import { useYoutubeApi } from "../context/useYoutubeApi";
+import { ChannelInfoProps } from "./ChannelInfo";
+import VideoList from "./VideoList";
+
+export default function RelatedVideos({ id }: ChannelInfoProps) {
+    const youtube = useYoutubeApi()
+    const { error, isLoading, data: videos } = useQuery({
+        queryKey: ['channelPlaylist', id],
+        queryFn: () => youtube?.search()
+    });
+    return (
+        <>
+            {isLoading && <p>Loading...</p>}
+            {error && <p>Something wrong...</p>}
+            {videos && <ul>
+                {
+                    videos.map((vid) => {
+                        return <VideoList key={vid.id} vid={vid} type='list' />
+
+                    })
+                }
+            </ul>}
+        </>
+    )
+}
