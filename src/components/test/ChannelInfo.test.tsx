108fe7746ab6a3004d2e4be302a9f4afa115e96c
gureumwoon <vfrvfr3221@gmail.com>
2024-06-28 13:33:25 +0900

    반복되는 테스트 코드 리팩토링

--- src/components/test/ChannelInfo.test.tsx
+++ src/components/test/ChannelInfo.test.tsx
@@ -12,12 +12,8 @@ describe('ChannelInfo', () => {
 
     it('renders correctly', async () => {
         fakeYoutube.channels.mockImplementation(() => 'url');
-        const { asFragment } = render(withAllContexts(
-            withRouter(
-                <Route path="/" element={<ChannelInfo id="id" name="channel" />} />
-            ),
-            fakeYoutube
-        ))
+        const { asFragment } = renderChannelInfo();
+
         await waitFor(() => screen.getByRole('img'));
         expect(asFragment()).toMatchSnapshot();
     });
@@ -26,27 +22,25 @@ describe('ChannelInfo', () => {
         fakeYoutube.channels.mockImplementation(() => {
             throw new Error('error');
         });
+        renderChannelInfo();
 
-        render(
-            withAllContexts(
-                withRouter(
-                    <Route path="/" element={<ChannelInfo id="id" name="channel" />} />
-                ),
-                fakeYoutube
-            ));
         expect(screen.queryByRole('img')).toBeNull();
     });
 
     // 위 snapshot테스트와 중복이 되어서 생략해도 가능하지만, 좀 더 명시적으로 나타내고 싶다면 테스트 추가해도 괜찮. 
     it('renders with URL', async () => {
         fakeYoutube.channels.mockImplementation(() => 'url');
+        renderChannelInfo()
 
-        render(withAllContexts(
+        await waitFor(() => expect(screen.getByRole('img')).toBeInTheDocument());
+    });
+
+    function renderChannelInfo() {
+        return render(withAllContexts(
             withRouter(
                 <Route path="/" element={<ChannelInfo id="id" name="channel" />} />
             ),
             fakeYoutube
         ));
-        await waitFor(() => expect(screen.getByRole('img')).toBeInTheDocument());
-    });
+    }
 })
