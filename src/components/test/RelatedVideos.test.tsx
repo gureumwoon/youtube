303b47068d274459310444894c7bf15c0188f91d
gureumwoon <vfrvfr3221@gmail.com>
2024-06-28 23:56:23 +0900

    renders related videos correctly 테스트 코드 작성

--- src/components/test/RelatedVideos.test.tsx
+++ src/components/test/RelatedVideos.test.tsx
@@ -1,6 +1,6 @@
 import { Route } from "react-router-dom";
 import { withAllContexts, withRouter } from "../../test/utils";
-import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
+import { render, screen, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
 import RelatedVideos from "../RelatedVideos";
 import { fakeVideos as videos } from "../../test/videos";
 
@@ -19,7 +19,23 @@ describe('RelatedVideos', () => {
             ),
             fakeYoutube
         ));
+
         await waitForElementToBeRemoved(screen.getByText('Loading...'));
         expect(asFragment()).toMatchSnapshot();
-    })
+    });
+
+    it('renders related videos correctly', async () => {
+        fakeYoutube.getChannelPlaylist.mockImplementation(() => videos);
+        render(withAllContexts(
+            withRouter(
+                <Route path="/" element={<RelatedVideos id="id" />} />
+            ),
+            fakeYoutube
+        ));
+
+        expect(fakeYoutube.getChannelPlaylist).toHaveBeenCalledWith('id');
+        await waitFor(() => {
+            expect(screen.getAllByRole('listitem')).toHaveLength(videos.length)
+        });
+    });
 })
