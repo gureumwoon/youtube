0b6340a7e7c1f77dfbea05d3eaa8f713d2402395
gureumwoon <vfrvfr3221@gmail.com>
2024-07-06 01:07:23 +0900

    withRouter 함수에 InitialEntry 타입 추가

--- src/test/utils.tsx
+++ src/test/utils.tsx
@@ -1,10 +1,13 @@
 import { ReactNode } from "react";
+import { LocationDescriptorObject } from 'history';
 import { MemoryRouter, Routes } from "react-router-dom";
 import { YoutubeApiContext } from "../context/YoutubeApiContext";
 import YoutubeApi from "../../api/YoutubeApi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export function withRouter(routes: ReactNode, initialEntry = '/') {
+type InitialEntry = string | LocationDescriptorObject;
+
+export function withRouter(routes: ReactNode, initialEntry: InitialEntry = '/') {
     return <MemoryRouter initialEntries={[initialEntry]}>
         <Routes>
             {routes}
