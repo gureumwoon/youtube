5dd3d1383a4022e940255ddfc2f6f3949a02704f
gureumwoon <vfrvfr3221@gmail.com>
2024-07-11 21:00:19 +0900

    사용하지 않는 변수에는 언더스코어 사용하도록 eslint에 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,7 @@ module.exports = {
     'jest'
   ],
   rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    baseUrl: 'http://localhost:5173/',
+    setupNodeEvents(_on, _config) {
       // implement node event listeners here
     },
   },
