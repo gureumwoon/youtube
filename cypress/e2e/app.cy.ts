450c9f70721ce08465a4868a5eacb96dc1f952ae
gureumwoon <vfrvfr3221@gmail.com>
2024-07-12 20:42:28 +0900

    첫 렌더링 페이지의 text 사용해서 exist하는지 E2E 테스트 코드 작성

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,7 +1,12 @@
 /// <reference types="cypress" />
+import '@testing-library/cypress/add-commands'
 
 describe('Youtube Clone', () => {
     beforeEach(() => {
         cy.visit('/')
-    })
+    });
+
+    it('renders', () => {
+        cy.findByText('YouTube').should('exist');
+    });
 })
