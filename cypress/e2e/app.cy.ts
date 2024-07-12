1d82f88f9839547dbb31b5c7d69bd016f8ebd862
gureumwoon <vfrvfr3221@gmail.com>
2024-07-12 22:09:30 +0900

    첫 페이지 render시 보여지는 popular비디오 테스트

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -3,10 +3,21 @@ import '@testing-library/cypress/add-commands'
 
 describe('Youtube Clone', () => {
     beforeEach(() => {
-        cy.visit('/')
+        cy.intercept('GET', /(mostPopular)/g, {
+            fixture: 'popular.json'
+        });
+        cy.intercept('GET', /(search)/g, {
+            fixture: 'search.json'
+        });
+        cy.viewport(1200, 800);
+        cy.visit('/');
     });
 
     it('renders', () => {
         cy.findByText('YouTube').should('exist');
+    });
+
+    it('shows popular video firs', () => {
+        cy.findByText('Popular Video').should('exist');
     });
 })
