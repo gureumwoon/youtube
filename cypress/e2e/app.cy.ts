66dc78116817b963433a4f3d2ccebaf47f99c647
gureumwoon <vfrvfr3221@gmail.com>
2024-07-12 22:44:49 +0900

    detail page rendering E2E 테스트 코드 작성

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -26,4 +26,11 @@ describe('Youtube Clone', () => {
         cy.findByRole('button').click();
         cy.findByText('Search Result1').should('exist');
     });
+
+    it('goes to detail page', () => {
+        cy.findAllByRole('listitem').first().click();
+        cy.findByTitle('Popular Video').should('exist');
+        cy.findByText('Popular Video').should('exist');
+        cy.findByText('Search Result1').should('exist');
+    })
 })
